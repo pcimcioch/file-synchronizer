b163bbfb93ef004156bff075de60d8d4b2bd05a7
Przemyslaw Cimcioch <cimcioch.przemyslaw@gmail.com>
2019-11-23 16:37:24 +0100

    File traversal working

--- script/vue/local-filesystem.js
+++ script/vue/local-filesystem.js
@@ -1,3 +1,4 @@
+// TODO: info for files
 Vue.component('dir-handler-info', {
   props: ['handler'],
   template: `
@@ -17,13 +18,17 @@ Vue.component('dir-handler-list', {
   props: ['handlers'],
   data: function() {
     return {
+      // TODO: this should be whole object, not only id
       selected: null
-    }
+    };
   },
   methods: {
     select: function(id) {
       this.selected = id;
       this.$emit('select', id)
+    },
+    open: function(id) {
+      this.$emit('open', id)
     }
   },
   template: `
@@ -32,8 +37,79 @@ Vue.component('dir-handler-list', {
               v-for="entry in handlers" 
               v-bind:key="entry.id"
               v-bind:class="{active: entry.id === selected}" 
-              v-on:click="select(entry.id)">
-        <span class="fas fa-folder" v-bind:title="entry.name">&nbsp;{{entry.name}}</span>
+              v-on:click="select(entry.id)"
+              v-on:dblclick="open(entry.id)">
+        <span class="fas fa-database" v-bind:title="entry.name">&nbsp;{{entry.name}}</span>
+      </button>
+    </div>
+  `
+});
+// TODO: move all components to separate files
+
+Vue.component('dir-handler', {
+  props: ['handler'],
+  data: function() {
+    return {
+      selected: null,
+      files: [{name: 'test'}],
+      path: []
+    };
+  },
+  methods: {
+    select: function(file) {
+      this.selected = file;
+      this.$emit('select', file)
+    },
+    open: function(file) {
+      if (!file.isDirectory) {
+        return;
+      }
+
+      this.path.push(file);
+      this.listDirectory();
+    },
+    listDirectory: async function() {
+      this.files = [];
+      this.select(null);
+      const directory = this.path[this.path.length - 1];
+
+      for await (const entry of directory.getEntries()) {
+        this.files.push(entry);
+      }
+    },
+    back: function() {
+      if (this.path.length === 1) {
+        this.$emit('close');
+      } else {
+        this.path.pop();
+        this.listDirectory();
+      }
+    }
+  },
+  watch: {
+    handler: function(newValue) {
+      this.path = [newValue.handler];
+      this.listDirectory();
+    }
+  },
+  template: `
+    <div class="list-group list-group-flush">
+      <button class="list-group-item list-group-item-action p-1" 
+              v-on:dblclick="back">
+        <span class="fas fa-arrow-left" title="Back">
+          &nbsp;Back
+        </span>
+      </button>
+      <button class="list-group-item list-group-item-action p-1" 
+              v-for="file in files" 
+              v-bind:key="file.name"
+              v-bind:class="{active: file === selected}" 
+              v-on:click="select(file)"
+              v-on:dblclick="open(file)">
+        <span v-bind:title="file.name">
+          <i class="far" v-bind:class="file.isFile ? 'fa-file text-info' : 'fa-folder text-warning'"></i>
+          &nbsp;{{file.name}}
+        </span>
       </button>
     </div>
   `
@@ -43,61 +119,59 @@ Vue.component('dir-handler-list', {
 Vue.component('local-filesystem', {
   data: function() {
     return {
-      dirHandlers: [
+      handlers: [
         {
-        id: 1,
-        name: 'File 1',
-        handler: null
-      }, {
-        id: 2,
-        name: 'File 2',
-        handler: null
-      }, {
-        id: 3,
-        name: 'File 3',
-        handler: null
-      }],
-      selected: {
-        id: null,
-        name: '',
-        handler: null
-      }
+          id: 1,
+          name: 'File 1',
+          handler: null
+        }, {
+          id: 2,
+          name: 'File 2',
+          handler: null
+        }, {
+          id: 3,
+          name: 'File 3',
+          handler: null
+        }],
+      selected: null,
+      opened: null
     }
   },
   methods: {
-    addDirHandler: function() {
-      const self = this;
-      window.chooseFileSystemEntries({type: 'openDirectory'}).then(function(handler) {
-        self.dirHandlers.push({
-          id: handler.name,
-          name: handler.name,
-          handler: handler
-        });
+    addDirHandler: async function() {
+      const handler = await window.chooseFileSystemEntries({type: 'openDirectory'});
+      this.handlers.push({
+        id: handler.name,
+        name: handler.name,
+        handler: handler,
       });
     },
     removeDirHandler: function() {
-      this.dirHandlers = this.selected ? this.dirHandlers.filter(e => e.id !== this.selected.id) : this.dirHandlers;
-      this.selected = {
-        id: null,
-        name: '',
-        handler: null
-      };
+      this.handlers = this.selected ? this.handlers.filter(e => e.id !== this.selected.id) : this.handlers;
+      this.selected = null;
     },
     select: function(id) {
-      this.selected = this.dirHandlers.find(el => el.id === id);
+      this.selected = this.handlers.find(el => el.id === id);
+    },
+    open: function(id) {
+      this.opened = this.handlers.find(el => el.id === id);
     }
   },
   template: `
     <div class="card">
       <div class="card-body">
-        <div class="row pb-2">
-            <button class="btn btn-sm btn-primary fas fa-plus" v-on:click="addDirHandler"></button>
-            <button class="btn btn-sm btn-danger fas fa-minus" v-on:click="removeDirHandler" v-bind:class="{disabled: !selected.id}"></button>
+        <!-- TODO: Those buttons and methods moved to dir-handler-list. 'handlers' passed as model -->
+        <div class="row pb-2 btn-group btn-group-sm" role="group">
+            <button class="btn btn-primary fas fa-plus" v-on:click="addDirHandler"></button>
+            <button class="btn btn-danger fas fa-minus" v-on:click="removeDirHandler" v-bind:disabled="!selected"></button>
         </div>
     
         <div class="row">
-          <dir-handler-list class="col-6" v-bind:handlers="dirHandlers" v-on:select="select"></dir-handler-list>
+          <dir-handler-list class="col-6" v-bind:handlers="handlers" v-on:select="select" v-on:open="open"></dir-handler-list>
           <dir-handler-info class="col-6" v-bind:handler="selected"></dir-handler-info>
+        </div>
+        <div class="row">
+          <dir-handler class="col-6" v-bind:handler="opened"></dir-handler>
         </div>
       </div>
     </div>
